1cb9a0b1277006513ff9c887bcda7ac12e7baa07
anilkumar2809 <anilkumaryalla88@gmail.com>
2022-02-27 18:26:46 -0500

    structure and store data inside config.JSON

--- commands/init.js
+++ commands/init.js
@@ -1,8 +1,6 @@
 const chalk = require('chalk');
 const path = require('path');
 const createVM = require('../lib/provision')
-const child  = require("child_process");
-const VBoxManage = require('../lib/VBoxManage');
 
 
 exports.command = 'init';
@@ -20,32 +18,6 @@ exports.handler = async argv => {
         createVM.mac(name);
     } 
     else {
-        child.execSync("bakerx run pj focal", {stdio: 'inherit'});
-        let state = await VBoxManage.show(name);
-        console.log(`VM is currently: ${state}`);
-        console.log(`VM is currently: ${state}`);
-        let info_cmd = 'bakerx ssh-info --format config pj'
-        const info = child.execSync(info_cmd).toString();
-        console.log("Info", info)
-        info_split = info.split("\n");
-        for(let i=0; i<info_split.length; i++){
-            if(i!=0){
-                temp = info_split[i].split(' ')
-                if(temp.length == 6){
-                    key = temp[4]
-                    value = temp[5]
-                    d[key] = value
-                }
-            }
-        }
-        console.log('information after retrieving', d)
-        var jsonData = JSON.stringify(d);
-        var fs = require('fs');
-        fs.writeFile("config.JSON", jsonData, function(err) {
-            if (err) {
-                console.log(err);
-            }
-        
-});
+        createVM.windows(name);   
     }
 };
