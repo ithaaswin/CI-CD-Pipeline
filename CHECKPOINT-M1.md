356a9b5bdb6e1384d983ffab20f92d1301036d19
ithaaswin <aitha@ncsu.edu>
2022-02-27 14:33:45 -0500

    Commiting m1 changes for init

--- CHECKPOINT-M1.md
+++ CHECKPOINT-M1.md
@@ -1,1 +1,20 @@
 # Link to the google doc - https://docs.google.com/document/d/1bllr1XzT1ITnWrJLnlQhKb7howaiI0E-bwBZg5gBw6s/edit
+
+-- Aswin
+For MAC - M1
+
+Error -- Basicvm was not detected when init file was run
+Fix -- changed the binary name from basicvm to vm in the init file
+
+Error -- .env file missing
+Fix -- Created .env file in the main repo
+
+Error -- When the vm with a name is already existing, new vm is not created and process is stopped
+Fix -- Introduced new lines to stop and delete the old vm with same name
+
+Enhancement -- When the vm with the default name is not available, it is spitting vm no available in to the console
+Fix -- Used 'pipe' to avoid pushing the default lines.
+
+
+
+
--- commands/init.js
+++ commands/init.js
@@ -1,5 +1,7 @@
 const chalk = require('chalk');
 const path = require('path');
+const m1 = require('./mac.js')
+const child  = require("child_process");
 
 exports.command = 'init';
 exports.desc = 'Prepare tool';
@@ -11,7 +13,16 @@ exports.builder = yargs => {
 
 exports.handler = async argv => {
     const { processor } = argv;
+    console.log(processor);
 
-    console.log(chalk.green("Preparing computing environment..."));
-   
+    if(processor == 'Arm64' ) {            
+        try{child.execSync("vm stop v1",{stdio: 'pipe'});}
+        catch{}
+        try{child.execSync("vm rm v1",{stdio: 'pipe'});}
+        catch{}
+        child.execSync("vm run v1 ubuntu:focal",{stdio: 'inherit'});
+        child.execSync("vm ssh-config v1 > config.txt")
+    } else {
+        docker.something
+    }
 };
