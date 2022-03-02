dcc30a288ecf30cd5f6e960504678c33de8a47eb
ayalla <ayalla@ncsu.edu>
2022-03-02 05:46:36 -0500

    installing tools and itrust on Vm for windows

--- commands/build.js
+++ commands/build.js
@@ -1,6 +1,6 @@
 const chalk = require("chalk");
 // const path = require("path");
-const vm_ssh =require("../lib/builder")
+const builder =require("../lib/builder")
 exports.command = 'build <job_name> <build_path>';
 exports.desc = 'runs the build command and executes the job from given file';
 
@@ -10,9 +10,9 @@ exports.handler = async argv => {
     console.log(build_path);
     console.log(processor);
     if(processor == 'Arm64' ) {            
-        vm_ssh.mac(job_name, build_path);
+        builder.mac(job_name, build_path);
     } 
     else {
-        vm_ssh.windows(job_name, build_path);   
+        builder.windows(job_name, build_path);   
     }
 };
