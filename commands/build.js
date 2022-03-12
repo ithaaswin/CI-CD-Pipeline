336ad31b8deb9d7e87de1db0ea53ca8443e2a381
harish hasti <hhasti@ncsu.edu>
2022-03-11 23:27:30 -0500

    fixed build command node

--- commands/build.js
+++ commands/build.js
@@ -1,7 +1,10 @@
 const builder =require("../lib/builder")
 exports.command = 'build <job_name> <build_path>';
 exports.desc = 'runs the build command and executes the job from given file';
-
+exports.builder = yargs => {
+    yargs.options({
+    });
+};
 exports.handler = async argv => {
     const{job_name, build_path, processor} = argv;
     console.log('Job Name', job_name);
