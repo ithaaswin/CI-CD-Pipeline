7e3a8eb4e39c6fe3b8a1aafcc2c7bb63473706ba
Aswin Itha <aitha@ncsu.edu>
2022-04-17 02:15:22 -0400

    Added mutation coverage

--- mutation/mutate.sh
+++ mutation/mutate.sh
@@ -10,6 +10,8 @@ repoName=$(basename $1 .git)
 rm -rf $repoName
 git clone --recursive $1
 
+rm -rf $2/mutation/Images $2/mutation/result.json
+
 cd ~
 sudo rm -rf result.json temp1.json temp2.json Images
 touch result.json temp1.json temp2.json
@@ -43,77 +45,90 @@ changeCounter=0
 
 for (( i=1; i<=$3; i++ ))
 do
-        cd ~
-        rm -rf 
-        mutateFile=$( node randomSelector.js ~/$repoName)
-        sudo rm -rf rewriteLog.txt
-        node rewrite.js $4 $repoName $mutateFile >> rewriteLog.txt
+NoChangeFiles=""
+changeFiles=""
+exceptionFiles=""
 
-        operation=$( cat rewriteLog.txt | head -n 1 )
-        operator="${mutateFile}"
-        operator+=": "
-        operator+="${operation}"
-        lenSourceLine=$(wc -l < rewriteLog.txt )
-        if [[ lenSourceLine -eq 2 ]]
-        then
-        sourceLine=$( cat rewriteLog.txt | tail -n 1 )
-        else
-        sourceLine="No Line Changed"
-        fi
+cd ~
+mutateFile=$( node randomSelector.js ~/$repoName)
+mutateFile=marqdown.js
+sudo rm -rf rewriteLog.txt
+node rewrite.js $4 $repoName $mutateFile >> rewriteLog.txt
 
-        pixelDiff=0
-        exceptionFlag=false
-        cd ~/$repoName
-        node index.js > serviceLog.txt 2>&1 &
-        process=$!
-        sudo mkdir -m 777 -p ~/Images/mutated/$i
-        sudo mkdir -m 777 -p ~/Images/difference/$i
+operation=$( cat rewriteLog.txt | head -n 1 )
+operator="${mutateFile}"
+operator+=": "
+operator+="${operation}"
+lenSourceLine=$(wc -l < rewriteLog.txt )
+if [[ lenSourceLine -eq 2 ]]
+then
+sourceLine=$( cat rewriteLog.txt | tail -n 1 )
+else
+sourceLine="No Line Changed"
+fi
 
-        cd ~
-        totSnaps=$(jq '.snaps | length' snapshots.json)
-        for (( j=0; j<$totSnaps; j++))
-        do
-        cd ~
-        snapFlag=false
-        snapName=$(jq '.snaps['$j'].name' snapshots.json | tr -d '"')
-        snapURL=$(jq '.snaps['$j'].url' snapshots.json | tr -d '"')
-        {
-                node screenshot.js $snapURL ~/Images/mutated/$i/$snapName
-        } || {
-                exceptionFlag=true
-                snapFlag=true
-        }
-        if $snapFlag
-        then
-        cd ~/$repoName
-        echo "Here $(cat serviceLog.txt)"
-        cp serviceLog.txt ~/Images/difference/$snapName.txt
-        node index.js > serviceLog.txt 2>&1 &
-        process=$!
-        else
-        compare -metric AE -fuzz 5% ~/Images/original/$snapName.png ~/Images/mutated/$i/$snapName.png ~/Images/difference/$i/$snapName.png  2>pixelDiffTemp
-        pixelDiff=$(( $(head -n 1 pixelDiffTemp)+$pixelDiff))
-        fi
-        done
-        
-        kill -9 $process > /dev/null
-        wait $process 2>/dev/null
+pixelDiff=0
+exceptionFlag=false
+cd ~/$repoName
+node index.js > serviceLog.txt 2>&1 &
+process=$!
+sudo mkdir -m 777 -p ~/Images/mutated/$i
+sudo mkdir -m 777 -p ~/Images/difference/$i
 
-        if $exceptionFlag
-        then
-        endResult='Exception'
-        exceptionCounter=$(($exceptionCounter+1))
-        exceptionFlag=false
-        elif [ $pixelDiff -eq 0 ]
-        then
-        endResult='Not Changed'
-        else
-        endResult='Changed'
-        changeCounter=$(($changeCounter+1))
-        fi
-                
-        json_data=$(cat <<EOF 
-{"$i":{"operator": "$operator","sourceLine": "$sourceLine","result": "$endResult"}}
+cd ~
+totSnaps=$(jq '.snaps | length' snapshots.json)
+echo ""
+echo "****************************************** Starting Mutations **************************************"
+for (( j=0; j<$totSnaps; j++))
+do
+cd ~
+snapFlag=false
+snapName=$(jq '.snaps['$j'].name' snapshots.json | tr -d '"')
+snapURL=$(jq '.snaps['$j'].url' snapshots.json | tr -d '"')
+{
+        node screenshot.js $snapURL ~/Images/mutated/$i/$snapName
+} || {
+        exceptionFlag=true
+        snapFlag=true
+        exceptionFiles+="${snapName}.md "
+}
+if $snapFlag
+then
+cd ~/$repoName
+cp serviceLog.txt ~/Images/difference/$i/$snapName.txt
+node index.js > serviceLog.txt 2>&1 &
+process=$!
+else
+compare -metric AE -fuzz 5% ~/Images/original/$snapName.png ~/Images/mutated/$i/$snapName.png ~/Images/difference/$i/$snapName.png  2>pixelDiffTemp
+pixelDiffTemp=$(head -n 1 pixelDiffTemp)
+if [ $pixelDiffTemp -eq 0 ]
+then
+NoChangeFiles+="${snapName}.md "
+else
+changeFiles+="${snapName}.md "
+fi
+pixelDiff=$(( $pixelDiffTemp+$pixelDiff))
+fi
+done
+
+kill -9 $process > /dev/null
+wait $process 2>/dev/null
+
+if $exceptionFlag
+then
+endResult='Exception'
+exceptionCounter=$(($exceptionCounter+1))
+exceptionFlag=false
+elif [ $pixelDiff -eq 0 ]
+then
+endResult='Not Changed'
+else
+endResult='Changed'
+changeCounter=$(($changeCounter+1))
+fi
+           
+json_data=$(cat <<EOF 
+{"$i":{"operator": "$operator","sourceLine": "$sourceLine","No Change Files": "$NoChangeFiles","Changed Files": "$changeFiles","Exception Files": "$exceptionFiles","result": "$endResult"}}
 EOF
 )
 
@@ -124,7 +139,7 @@ jq -s add temp1.json temp2.json > result.json
 
 cd ~/$repoName
 git reset --hard > /dev/null 2>&1 &
-git clean -f -d
+git clean -f -d > /dev/null 2>&1 &
 
 echo "Mutation-$i Completed"
 done
@@ -135,7 +150,15 @@ cp ~/result.json $2/mutation
 passedCounter=$(($3-$changeCounter-$exceptionCounter))
 denom=$(( $3-$exceptionCounter ))
 
-echo "Failed Mutants: $changeCounter"
-echo "Passed Mutants: $passedCounter"
-echo "Exception Mutants: $exceptionCounter"
-echo "Mutation Coverage: $changeCounter/$denom"
+rm -rf $2/mutation/mutationCoverage.txt mutationCoverage.txt
+echo "*************************************************************************************************" >> mutationCoverage.txt
+echo "********************************** Mutation Coverage Results ************************************" >> mutationCoverage.txt
+echo "Failed Mutants: $changeCounter" >> mutationCoverage.txt
+echo "Passed Mutants: $passedCounter" >> mutationCoverage.txt
+echo "Exception Mutants: $exceptionCounter" >> mutationCoverage.txt
+echo "Mutation Coverage: $changeCounter/$denom" >> mutationCoverage.txt
+echo "*************************************************************************************************" >> mutationCoverage.txt
+
+cp mutationCoverage.txt $2/mutation/mutationCoverage.txt
+echo ""
+cat mutationCoverage.txt
