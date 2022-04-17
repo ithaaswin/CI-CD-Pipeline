bd749dac466c52eb0bdb85815ea2e49ffff12c2f
ayalla <ayalla@ncsu.edu>
2022-04-17 11:32:32 -0400

    Update randomSelector.js

--- mutation/randomSelector.js
+++ mutation/randomSelector.js
@@ -4,11 +4,20 @@ var files = fs.readdirSync(process.argv[2]);
 var js_files_in_directory = []
 for(let i=0; i < files.length; i++){
     if(files[i].split('.')[1] == 'js'){
+        let b="index.js"
+        let result=strcmp(files[i], b);
+        if(result===0){
+            continue
+        }
         js_files_in_directory.push(files[i])
 
     }
 }
-
+function strcmp(a, b) {
+    if (a.toString() < b.toString()) return -1;
+    if (a.toString() > b.toString()) return 1;
+    return 0;
+}
 js_json = {}
 
 for(let j=0; j < js_files_in_directory.length; j++){
