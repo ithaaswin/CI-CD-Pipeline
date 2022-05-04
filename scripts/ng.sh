8a3f407f366c10fccc31050b8c6a7c7579c98a8e
Aswin Itha <aitha@ncsu.edu>
2022-05-04 17:02:55 -0400

    processing functions dynamic

--- scripts/ng.sh
+++ scripts/ng.sh
@@ -8,24 +8,31 @@
 # $7 - Threshold
 # $8 - JSON File Name
 
-grep "'"Statements"'" coverageResult.txt | xargs -n 1 > coverage.txt
-thresholdFound=$( head -n 3 coverage.txt | tail -n 1 | cut -d% -f 1 )
+grep "Statements" coverageResult.txt | xargs -n 1 > coverage.txt
+statVal=$( head -n 3 coverage.txt | tail -n 1 | cut -d% -f 1 )
+grep "Branches" coverageResult.txt | xargs -n 1 > coverage.txt
+branchVal=$( head -n 3 coverage.txt | tail -n 1 | cut -d% -f 1 )
+grep "Functions" coverageResult.txt | xargs -n 1 > coverage.txt
+funcVal=$( head -n 3 coverage.txt | tail -n 1 | cut -d% -f 1 )
+grep "Lines" coverageResult.txt | xargs -n 1 > coverage.txt
+lineVal=$( head -n 3 coverage.txt | tail -n 1 | cut -d% -f 1 )
+thresholdFound=$( echo "$statVal $branchVal $funcVal $lineVal" | awk '{print $1 + $2 + $3 + $4}' | awk '{print $1/4}' )
 
-if [ $thresholdFound<$7 ]
+if (( $(echo "$thresholdFound > $7"|bc -l) ))
 then
-sudo python3 mail.py $1 $2 $3 $4 $5 "False"
-json_data=$(cat <<EOF 
-{"$8":"Not Permitted"}
-EOF
-)
-else
 sudo python3 mail.py $1 $2 $3 $4 $5 "True"
 json_data=$(cat <<EOF 
 {"$8":"Permitted"}
 EOF
 )
+else
+sudo python3 mail.py $1 $2 $3 $4 $5 "False"
+json_data=$(cat <<EOF 
+{"$8":"Not Permitted"}
+EOF
+)
 fi
-echo "$8Permission"
+
 touch $8Permission.json
 echo $json_data > temp1.json
 cat $8Permission.json > temp2.json
